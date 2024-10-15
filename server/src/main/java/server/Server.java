b1af8fc13fda96f48424ad9a28d0702c25c09b6d
brownerfox <taftjl123@gmail.com>
2024-10-15 17:28:45 -0600

    added the beginning parts of the endpoints to the Server.java file

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import dataaccess.DataAccessException;
 import spark.*;
 
 public class Server {
@@ -8,6 +9,17 @@ public class Server {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
+
+
+        Spark.post("/user", this::createUser);
+        Spark.post("/session", this::loginUser);
+        Spark.delete("/session", this::logoutUser);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/db", this::clear);
+        Spark.exception(DataAccessException.class, this::exceptionHandler);
+
 
         // Register your endpoints and handle exceptions here.
         //is line initializes the server and can be removed once you have a functioning endpoint
@@ -18,8 +30,42 @@ public class Server {
         return Spark.port();
     }
 
+
+
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private void exceptionHandler(DataAccessException ex, Request req, Response res) {
+        res.status(ex.StatusCode());
+    }
+
+    private Object createUser(Request req, Response res) throws DataAccessException {
+        return authToken;
+    }
+
+    private Object loginUser(Request req, Response res) throws DataAccessException {
+        return authToken;
+    }
+
+    private Object logoutUser(Request req, Response res) throws DataAccessException {
+        return "";
+    }
+
+    private Object listGames(Request req, Response res) throws DataAccessException {
+        return gameList;
+    }
+
+    private Object createGame(Request req, Response res) throws  DataAccessException {
+        return gameData;
+    }
+
+    private Object joinGame(Request req, Response res) throws  DataAccessException {
+        return "";
+    }
+
+    private Object clear(Request req, Response res) throws DataAccessException {
+        return "";
     }
 }
