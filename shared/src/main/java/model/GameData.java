5f3110020c0737b078b97576d9df4154ff89783c
brownerfox <taftjl123@gmail.com>
2024-10-23 00:21:21 -0600

    removed unused methods in GameData record

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -3,14 +3,6 @@ package model;
 import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-    GameData addBlackPlayer (String username) {
-        return new GameData(gameID, whiteUsername, username, gameName, game);
-    }
-
-    GameData addWhitePlayer (String username) {
-        return new GameData(gameID, username, blackUsername, gameName, game);
-    }
-
     GameData updateGame (ChessGame newGame) {
         return new GameData(gameID, whiteUsername, blackUsername, gameName, newGame);
     }
