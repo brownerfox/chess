99d8f5a5a6679107fe17585a2163cb2d01325f9c
brownerfox <taftjl123@gmail.com>
2024-10-31 19:33:40 -0600

    Created placeholders for all of the test in DataAccessTests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,4 +1,124 @@
 package dataaccess;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class DataAccessTests {
+
+    @Test
+    @DisplayName("Create User Success")
+    public void createUserSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Create User Success")
+    public void createUserFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Get User Success")
+    public void getUserSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Get User Failure")
+    public void getUserFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Create Game Success")
+    public void createGameSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Create Game Failure")
+    public void createGameFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Get Game Success")
+    public void getGameSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Get Game Failure")
+    public void getGameFailure () {
+
+    }
+
+    @Test
+    @DisplayName("List Games Success")
+    public void listGamesSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("List Games Failure")
+    public void listGamesFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Update Game Success")
+    public void updateGameSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Update Game Failure")
+    public void updateGameFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Create Authorization Success")
+    public void createAuthSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Create Authorization Failure")
+    public void createAuthFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Get Authorization Success")
+    public void getAuthSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Get Authorization Failure")
+    public void getAuthFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Delete Authorization Success")
+    public void deleteAuthSuccess () {
+
+    }
+
+    @Test
+    @DisplayName("Delete Authorization Failure")
+    public void deleteAuthFailure () {
+
+    }
+
+    @Test
+    @DisplayName("Clear")
+    public void clearSuccess () {
+
+    }
+
+
 }
