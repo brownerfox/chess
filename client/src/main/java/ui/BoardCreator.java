4a4bee4122bd5a546fdfecd10e45656d2f2b0831
brownerfox <taftjl123@gmail.com>
2024-11-13 17:25:16 -0700

    Started working on the methods in the board creator class

--- client/src/main/java/ui/BoardCreator.java
+++ client/src/main/java/ui/BoardCreator.java
@@ -1,4 +1,21 @@
 package ui;
 
+import chess.ChessGame;
+
 public class BoardCreator {
+    ChessGame game;
+
+    public BoardCreator(ChessGame game) {
+        this.game = game;
+    }
+
+    public void updateGame(ChessGame game) {
+        this.game = game;
+    }
+
+    public void printBoard(ChessGame.TeamColor teamColor) {
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            
+        }
+    }
 }
