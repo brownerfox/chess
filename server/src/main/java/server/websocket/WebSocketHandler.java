70252a00aa7fe25877112c2eaeff0fb9530e868b
brownerfox <taftjl123@gmail.com>
2024-12-04 15:22:26 -0700

    Added some functionality that kind of cleaned up my code but also kind of made it ugly \_=.,,.=_/

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -2,12 +2,17 @@ package server.websocket;
 
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import results.ErrorResult;
+import service.BadGameIDException;
+import service.ServiceException;
+import websocket.commands.JoinGameCommand;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
@@ -42,26 +47,21 @@ public class WebSocketHandler {
     public void onMessage(Session session, String message) throws IOException {
 
         if (message.contains("\"commandType\":\"JOIN_PLAYER\"")) {
-
-        }
-
-        try {
-            UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
-            authData = dataAccess.getAuth(action.getAuthToken());
-            gameData = dataAccess.getGame(action.getGameID());
-            switch (action.getCommandType()) {
-                case JOIN_PLAYER -> joinPlayer(action);
-                case JOIN_OBSERVER -> joinObserver(action);
-                case MAKE_MOVE -> makeMove(action);
-                case LEAVE -> exit(action);
-                case RESIGN -> resign(action);
-            }
-        } catch (Exception e) {
-            sendError(session, new ErrorResult("Error: unauthorized"));
+            JoinGameCommand action = new Gson().fromJson(message,JoinGameCommand.class);
+            setData(session, action.getAuthToken(), action.getGameID());
+            joinPlayer(action);
+        } else if (message.contains("\"commandType\":\"MAKE_MOVE\"")) {
+            MakeMoveCommand action = new Gson().fromJson(message, MakeMoveCommand.class);
+            setData(session, action.getAuthToken(), action.getGameID());
+            makeMove(action);
+        } else {
+            UserGameCommand action = new Gson().fromJson(message,UserGameCommand.class);
+            setData(session, action.getAuthToken(), action.getGameID());
+            determineAction(action);
         }
     }
 
-    private void joinPlayer(UserGameCommand action) throws IOException {
+    private void joinPlayer(JoinGameCommand action) throws IOException {
         var outgoingMessage = String.format("%s is in the shop", authData.username());
         var notification = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
         connections.broadcast(notification);
@@ -76,7 +76,7 @@ public class WebSocketHandler {
         connections.broadcast(notification);
     }
 
-    private void makeMove(UserGameCommand action) {
+    private void makeMove(MakeMoveCommand action) {
 
     }
 
@@ -86,5 +86,30 @@ public class WebSocketHandler {
     private void sendError(Session session, ErrorResult error) throws IOException {
         System.out.printf("Error: %s%n", new Gson().toJson(error));
         session.getRemote().sendString(new Gson().toJson(error));
+    }
+
+    public void determineAction (UserGameCommand action) throws IOException {
+        switch (action.getCommandType()) {
+            case JOIN_OBSERVER -> joinObserver(action);
+            case LEAVE -> exit(action);
+            case RESIGN -> resign(action);
+        }
+    }
+
+    public void setData (Session session, String authToken, int gameID) throws IOException {
+        try {
+            setAuthData(authToken);
+            setGameData(gameID);
+        } catch (Exception e) {
+            sendError(session, new ErrorResult("Error: unauthorized"));
+        }
+    }
+
+    public void setAuthData (String authToken) throws DataAccessException, ServiceException {
+        authData = dataAccess.getAuth(authToken);
+    }
+
+    public void setGameData (int gameID) throws BadGameIDException, DataAccessException, ServiceException{
+        gameData = dataAccess.getGame(gameID);
     }
 }
